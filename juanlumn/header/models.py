58cf54ac3c87520fe2a12b7ad5ffcac80f92e021
juanlumn <juanlumn@gmail.com>
2015-11-03 23:19:53 +0000

    Changes in the models. Added main template folder and admin template

--- juanlumn/header/models.py
+++ juanlumn/header/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class HeaderLink(models.Model):
     header_link_text = models.CharField(max_length=200)
 
+    def __str__(self):
+    	return self.header_link_text
 
 class HeaderSubLink(models.Model):
     header_link = models.ForeignKey(HeaderLink)
     header_sub_link_text = models.CharField(max_length=200)
+
+    def __str__(self):
+    	return self.header_sub_link_text
